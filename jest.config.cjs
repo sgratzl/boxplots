389c02c0df8c186a0eb2c2f0de8ecd94f9a3352d
Samuel Gratzl <sam@sgratzl.com>
2024-06-08 18:57:35 -0400

    build: fix test setup

--- jest.config.cjs
+++ jest.config.cjs
@@ -1,6 +1,6 @@
-export default {
-  testEnvironment: 'jsdom',
-  preset: 'ts-jest/presets/js-with-ts-esm',
-  rootDir: './src',
+/* eslint-env node */
+
+module.exports = {
+  preset: 'ts-jest',
   testRegex: '((\\.|/)(test|spec))\\.[jt]sx?$',
 };
